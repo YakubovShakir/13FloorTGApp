7b47a5f035a360e9ee8f49a519d7ce308bdaacf7
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-01-05 14:53:53 +0300

    Треня не начинается если полная настроение

--- src/screens/ActionScreen/tabs/SkillTab.jsx
+++ src/screens/ActionScreen/tabs/SkillTab.jsx
@@ -21,7 +21,6 @@ import countPercentage from "../../../utils/countPercentage.js"
 import { useSettingsProvider } from "../../../hooks.jsx"
 
 
-
 const SkillTab = ({
   modalData,
   setModalData,
@@ -31,10 +30,10 @@ const SkillTab = ({
   userId,
 }) => {
   const [skills, setSkills] = useState(null) // List of skills
-  const [userLearningSkills, setUserLearningSkills] = useState(null) //  User already Learned skills
+  const [userLearningSkills, setUserLearningSkills] = useState(null) // User already Learned skills
   const [userLearnedSkills, setUserLearnedSkills] = useState(null) // User learning at this time skills
   const [trainingParamters, setTrainingParameters] = useState(null) // User training parameters
-  const [activeProcess, setActiveProcess] = useState(null) //  User active training
+  const [activeProcess, setActiveProcess] = useState(null) // User active training
 
   const { lang } = useSettingsProvider()
 
@@ -102,14 +101,15 @@ const SkillTab = ({
   }
 
   const { Icons } = Assets
+
   // Return skill if it already learned
   const checkLearnedSkill = (skillId) => {
     const learned = userLearnedSkills?.find(
       (skill) => skill?.skill_id === skillId
     )
-    console.log('learned', userLearnedSkills)
     return learned && skills?.find((skill) => skill?.skill_id === skillId)
   }
+  
   // Return skill if it in study
   const checkLearningSkill = (skillId) => {
     const learning = userLearningSkills?.find(
@@ -118,124 +118,22 @@ const SkillTab = ({
     return learning && skills?.find((skill) => skill?.skill_id === skillId)
   }
 
-  // Handle buy skill
-  const handleBuySkill = async (skill) => {
-    await startProcess("skill", userId, skill?.skill_id)
-    const userParameters = await getParameters(userId)
-    const userLearningSkills = await getProcesses("skill", userId)
-    console.log(userLearningSkills)
-
-    setUserParameters(userParameters)
-    setUserLearningSkills(userLearningSkills)
+  // Check if the training button should be active
+  const checkTrainingButtonActive = () => {
+    // If user's mood is 100%, disable the button
+    return userParameters?.mood < 100;
   }
 
-  useEffect(() => {
-    if (modalData?.type === "skill") {
-      const learning = userLearningSkills?.find(
-        (skill) => skill?.type_id === modalData?.id
-      )
-      if (learning) {
-        const learningSkill = skills?.find(
-          (skill) => skill?.skill_id === learning?.type_id
-        )
-        setModalData(setSkillModalData(learningSkill))
-      }
-    }
-  }, [userLearningSkills])
-
-  // Get status for buy skill
-  const getSkillBuyStatus = (skill) => {
-    const coins = userParameters?.coins >= skill?.coins_price
-    const learned = checkLearnedSkill(skill?.skill_id)
-    const learning = checkLearningSkill(skill?.skill_id)
-    const requiredSkill = checkLearnedSkill(skill?.skill_id_required)
-
-    if (coins && !learned && !learning && requiredSkill) return true
-  }
-
-  // Check active status (color) for skill card
-  const checkActiveSkillButton = (skill) => {
-    if (checkLearningSkill(skill?.skill_id)) return true
-    if (checkLearnedSkill(skill?.skill_id)) return false
-    if (userParameters?.coins < skill?.coins_price) return false
-    if (skill?.skill_id_required) {
-      if (!checkLearnedSkill(skill?.skill_id_required)) return false
-    }
-    return true
-  }
-
-  // Build modal data for skill card
-  const setSkillModalData = (skill) => {
-    const learned = checkLearnedSkill(skill?.skill_id)
-    const learning = userLearningSkills?.find(
-      (sk) => sk?.type_id === skill?.skill_id
-    )
-    const bottomButtonOnClick =
-      getSkillBuyStatus(skill) && (() => handleBuySkill(skill))
-
-    const data = {
-      type: "skill",
-      id: skill?.skill_id,
-      title: skill?.name,
-      image: skill?.link,
-      blocks: [
-        {
-          icon: Icons.balance,
-          text: translations.cost[lang],
-          value: skill?.coins_price,
-        },
-        {
-          icon: Icons.clock,
-          text: null,
-          fillPercent:
-            learning?.duration || learning?.seconds
-              ? countPercentage(
-                  learning?.duration * 60 + learning?.seconds,
-                  skill?.duration * 60
-                )
-              : false,
-          value:
-            learning?.duration || learning?.seconds
-              ? formatTime(learning?.duration, learning?.seconds)
-              : formatTime(skill?.duration),
-        },
-        skill?.skill_id_required && {
-          icon: skills?.find((sk) => sk?.skill_id === skill?.skill_id_required)
-            ?.link,
-          text: skills?.find((sk) => sk?.skill_id === skill?.skill_id_required)
-            ?.name,
-          fillPercent: "100%",
-          fillBackground: !checkLearnedSkill(skill?.skill_id_required)
-            ? "#4E1010" // red
-            : "#0E3228", // green
-        },
-      ].filter(Boolean),
-      buttons: [
-        {
-          icon: !(learned || learning) && Icons.balance,
-          text:
-            (learned && translations.learned[lang]) ||
-            (learning && translations.boost[lang]) ||
-            skill?.coins_price,
-          onClick: bottomButtonOnClick,
-          active: checkActiveSkillButton(skill),
-          bg:
-            (learned &&
-              "linear-gradient(180deg, rgba(233,78,27,1) 0%, rgba(243,117,0,1) 50%)") ||
-            (learning &&
-              "linear-gradient(180deg, rgba(233,78,27,1) 0%, rgba(243,117,0,1) 100%)"),
-        },
-      ],
-    }
-    return data
-  }
-
-  // Start training process
+  // Handle start training
   const handleStartTraining = async () => {
-    await startProcess("training", userId)
-    const activeProcess = await getActiveProcess(userId)
-    setActiveProcess(activeProcess)
-  }
+    if (checkTrainingButtonActive()) {
+      await startProcess("training", userId);
+      const activeProcess = await getActiveProcess(userId);
+      setActiveProcess(activeProcess);
+      // Navigate to the MainScreen after starting the process
+      navigation.navigate('MainScreen');
+    }
+  };
 
   // Get parameters for static training card
   const getItemTrainingParams = () => {
@@ -273,13 +171,10 @@ const SkillTab = ({
     return [
       {
         text: activeProcess?.type === "training" ? translations.inProgress[lang] : translations.start[lang],
-        active: true,
-        
-        onClick:
-          activeProcess?.type !== "training" && (() => {
-            handleStartTraining();
-            navigation.navigate('MainScreen'); // Переход на основной экран сразу после начала тренировки
-          }),
+        active: checkTrainingButtonActive(), // Set button active status based on mood
+        onClick: checkTrainingButtonActive() && (() => {
+          handleStartTraining();
+        }),
       },
     ];
   };
@@ -300,6 +195,7 @@ const SkillTab = ({
       console.log("Error when updateInfromation", e)
     }
   }
+
   useEffect(() => {
     const updater = updateProcessesTimers(
       userLearningSkills,
@@ -309,12 +205,14 @@ const SkillTab = ({
 
     return () => clearInterval(updater)
   }, [userLearningSkills])
+
   useEffect(() => {
     if (activeProcess?.type === "training") {
       const updater = updateProcessTimers(activeProcess, setActiveProcess)
       return () => clearInterval(updater)
     }
   }, [activeProcess])
+
   useEffect(() => {
     getSkills().then((r) => {
       setSkills(r)
@@ -327,21 +225,19 @@ const SkillTab = ({
   }, [])
 
   return (
-    <temCard>
+    <temCard >
       {/* User training static card*/}
       {trainingParamters && (
         <ItemCard
           ItemIcon={Icons.training}
           ItemTitle={translations.training[lang]}
-           ItemDescription={translations.trainingDesc[lang]}
+          ItemDescription={translations.trainingDesc[lang]}
           ItemParamsBlocks={getItemTrainingParams()}
           ItemButtons={getItemTrainingButton()}
           ItemIndex={0}
         />
       )}
-
-     
-    </temCard>
+    </temCard >
   )
 }
 
