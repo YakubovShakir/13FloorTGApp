ffff315e4e11888dc1417d1ac207ff9e0c4539a3
Leo <leo@MacBook-Pro-Leo.local>
2025-01-16 10:12:59 +0300

    attempt #228 to fullscreen

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -4,30 +4,30 @@ const Telegram = window.Telegram.WebApp
 
 const useTelegram = {
   setReady: () => {
-    Telegram?.ready()
-    Telegram.BackButton.hide()
-    Telegram.MainButton.hide()
     Telegram.expand()
     postEvent('web_app_expand')
+    Telegram?.ready()
   },
   setFullScreen: () => {
-    try {
-      // doesnt work in browsers
-      Telegram.expand()
-      postEvent('web_app_expand')
-    } catch(_) {}
+    // try {
+    //   // doesnt work in browsers
+    //   Telegram.expand()
+    //   postEvent('web_app_expand')
+    // } catch(_) {}
   },
   setBackButton: (callback) => {
-    Telegram.BackButton.onClick(callback)
-    Telegram.BackButton.show()
+    // Telegram.BackButton.onClick(callback)
+    // Telegram.BackButton.show()
   },
   getUserId: Telegram?.initDataUnsafe?.user?.id,
 
   hideBackButton: () => {
-    Telegram.BackButton.show()
-    Telegram.BackButton.hide()
+    // Telegram.BackButton.show()
+    // Telegram.BackButton.hide()
   },
-  setHeaderColor: (color) => Telegram.setHeaderColor(color),
+  setHeaderColor: (color) => {
+    // Telegram.setHeaderColor(color)
+  },
 }
 
 export default useTelegram
