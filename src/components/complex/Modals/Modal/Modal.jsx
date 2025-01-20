fac97a01990bc2b72d77148826f98d2a7a8eb9e4
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-01-20 13:47:51 +0300

    высота модалки

--- src/components/complex/Modals/Modal/Modal.jsx
+++ src/components/complex/Modals/Modal/Modal.jsx
@@ -9,7 +9,7 @@ const Modal = ({ bottom, left, width, height, data, onClose, logoWidth }) => {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       className="Modal"
-      style={{  left: left, width: width, height: "83%", zIndex: 10 }}
+      style={{  left: left, width: width, height: "90%", zIndex: 10 }}
     >
       
       <img
