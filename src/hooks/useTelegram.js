ccfc721a7a4726ee7ec80fb62382437eadeb44cf
Leonid Brown <leonid.brown01@gmail.com>
2025-02-01 11:48:26 +0700

    fix: fullscreen

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -8,7 +8,7 @@ const useTelegram = {
     try {
       console.log(WebApp.version)
       postEvent('web_app_expand')
-      // postEvent('web_app_request_fullscreen')
+      postEvent('web_app_request_fullscreen')
       postEvent('web_app_ready')
     } catch(err) {
       console.log(err)
