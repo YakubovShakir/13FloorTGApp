9403fba870075ca6ef9eff703af87f9de42f641d
Leonid Brown <leonid.brown01@gmail.com>
2025-02-10 13:14:47 +0700

    fix: loading envs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import * as dotenv from "dotenv"
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), EnvironmentPlugin('all', { loadEnvFiles: true })],
   server: {
     watch: {
       usePolling: true,
