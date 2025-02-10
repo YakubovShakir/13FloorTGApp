c3e75365f49f3f02d004a5d4793ccc73f332f4d6
Leonid Brown <leonid.brown01@gmail.com>
2025-02-10 12:53:01 +0700

    fix: dotenv

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import UserContext, { UserProvider } from "./UserContext.jsx"
+import { config } from "dotenv"
 import App from "./App.jsx"
 import "./index.css"
 import { NotificationProvider } from "./NotificationContext.jsx"
+
+config()
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
