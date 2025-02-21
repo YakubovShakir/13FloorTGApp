ab39b3dadcedb48dfbbfee3bcf6e0038e48d2d61
Leonid Brown <leonid.brown01@gmail.com>
2025-02-21 11:33:36 +0700

    feat: global stars util

---
+++ src/utils/handleStarsPayment.js
@@ -0,0 +1,21 @@
+import WebApp from "@twa-dev/sdk"
+import { instance } from "./services/instance"
+
+export const handleStarsPayment = async (userId, productType, productId) => {
+    const response = await instance.post('/users/request-stars-invoice-link', {
+        id: productId,
+        productType,
+        userId,
+    })
+    await new Promise((resolve) => {
+      WebApp.openInvoice(response.data.invoiceLink, (status) => {
+        // Можно вызвать попап или анимацию успеха/фейла
+        if(status === "paid") {
+            return resolve()
+        }
+        if(status === "cancelled") {}
+        if(status === 'pending') {}
+        if(status === 'failed') {}
+      })
+    })
+}
