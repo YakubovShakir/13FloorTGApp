93acb70874294ba8d29dbc4ba1b77d5e9421bcd8
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-03-11 17:04:34 +0300

    правка

--- src/components/section/ScreenContainer/ScreenContainer.jsx
+++ src/components/section/ScreenContainer/ScreenContainer.jsx
@@ -3,7 +3,7 @@ const ScreenContainer = ({ withTab, children }) => {
     // const height = withTab ? "82%" : "90%"
   return (
     <div
-      style={{ height: 'auto',  overflowY: "scroll", padding: "13% 0 5% 0"}}
+      style={{ height: 'auto',  overflowY: "scroll", padding: "15% 0 5% 0"}}
       className="ScreenContainer"
     >
       {children}
