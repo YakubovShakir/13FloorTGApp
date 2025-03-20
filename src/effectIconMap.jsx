bb7f74d137fa1fea42c2688c4cdd59c1222135ff
Leonid Brown <leonid.brown01@gmail.com>
2025-03-20 20:29:42 +0700

    fix: var

---
+++ src/effectIconMap.jsx
@@ -0,0 +1,38 @@
+import Assets from "./assets/index"
+
+export default {
+    cant_fall_below_percent: {
+      hungry: Assets.Icons.hungryUp,
+      energy: Assets.Icons.energyUp,
+      mood: Assets.Icons.moodUp,
+      coins: Assets.Icons.balance,
+    },
+    profit_hourly_percent: {
+      hungry: Assets.Icons.hungryUp,
+      energy: Assets.Icons.energyUp,
+      mood: Assets.Icons.moodUp,
+      coins: Assets.Icons.balance
+    },
+    cost_hourly_percent: {
+      hungry: Assets.Icons.hungryUp,
+      energy: Assets.Icons.energyUp,
+      mood: Assets.Icons.moodUp,
+      coins: Assets.Icons.balance
+    },
+    profit_per_tick_fixed: {
+      hungry: Assets.Icons.hungryUp,
+      energy: Assets.Icons.energyUp,
+      mood: Assets.Icons.moodUp,
+      coins: Assets.Icons.balance
+    },
+    cost_per_tick_fixed: {
+      hungry: Assets.Icons.hungryUp,
+      energy: Assets.Icons.energyUp,
+      mood: Assets.Icons.moodUp,
+      coins: Assets.Icons.balance
+    },
+    autostart: {
+      sleeping_when_energy_below: Assets.Icons.clock,
+      'default': Assets.Icons.clock,
+    }
+  }
