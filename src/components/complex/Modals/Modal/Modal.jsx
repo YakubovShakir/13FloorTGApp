1232fb21fce33cf27766506a444f5e813d5411f9
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-03-21 12:01:30 +0300

    правочка

--- src/components/complex/Modals/Modal/Modal.jsx
+++ src/components/complex/Modals/Modal/Modal.jsx
@@ -9,7 +9,7 @@ const Modal = ({ bottom, left, width, height, data, onClose, logoWidth }) => {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       className="Modal"
-      style={{  top: "120.5px", left: left, width: width, height: "100%", zIndex: 100 }}
+      style={{  top: "160.5px", left: left, width: width, height: "100%", zIndex: 100 }}
     >
       
       <img
