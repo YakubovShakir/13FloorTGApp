3f40b42b8ec7b7d79795835c321ee1aa5b2b4962
Leonid Brown <leonid.brown01@gmail.com>
2025-03-29 22:59:21 +0700

    fix: nft

--- src/screens/ShopScreen/tabs/NftTab.jsx
+++ src/screens/ShopScreen/tabs/NftTab.jsx
@@ -280,7 +280,7 @@ const NftTab = () => {
       const result = await tonConnectUI.sendTransaction(transaction, {
         modals: "all",
         notifications: "all",
-        skipRedirectToWallet: 'ios'
+        skipRedirectToWallet: 'never'
       });
 
       console.log("Transaction Result:", result);
