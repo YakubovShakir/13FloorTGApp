4278b3b107e7f3eec8d55d02a7b5718b04021fb2
Leonid Brown <leonid.brown01@gmail.com>
2025-03-30 23:09:09 +0700

    fix: vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import * as dotenv from "dotenv"
 import EnvironmentPlugin from 'vite-plugin-environment'
+import inject from "@rollup/plugin-inject"
 
 dotenv.config()
 // https://vitejs.dev/config/
@@ -13,7 +14,13 @@ export default defineConfig({
       useFsEvents: true
     }
   },
-  define: {
-    'process.env': {}
-  }
+  build: {
+		rollupOptions: {
+			plugins: [
+				inject({
+					process: "process"
+				})
+			]
+		}
+	}
 })
