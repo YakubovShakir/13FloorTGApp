a398b010f031ddcb4724457276596fecf220737e
Leonid Brown <leonid.brown01@gmail.com>
2025-03-30 17:50:57 +0700

    fix: spec

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,16 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import * as dotenv from "dotenv"
 import EnvironmentPlugin from 'vite-plugin-environment'
+import polyfillNode from 'rollup-plugin-polyfill-node'
 
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin('all', { loadEnvFiles: true })],
+  plugins: [react(),  polyfillNode(), EnvironmentPlugin('all', { loadEnvFiles: true })],
   server: {
     watch: {
       usePolling: true,
       useFsEvents: true
     }
   },
-  optimizeDeps: {
-    esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis'
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                buffer: true
-            })
-        ]
-    }
-}
 })
