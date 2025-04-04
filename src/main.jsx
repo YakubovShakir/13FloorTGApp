66f6e8c3d992caf13f91e5f67a91e5ccda0684d4
Leonid Brown <leonid.brown01@gmail.com>
2025-04-05 00:52:40 +0700

    hot-fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import App from "./App.jsx"
 import "./index.css"
 import { SettingsProvider } from "./hooks.jsx"
 import { UserProvider } from "./UserContext.jsx"
+import { EmojiReactionProvider } from "./EmojiReactionContext.jsx"
+import { NotificationProvider } from "./NotificationContext.jsx"
 
 createRoot(document.getElementById("root")).render(
-  <SettingsProvider>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </SettingsProvider>
+  <EmojiReactionProvider>
+    <NotificationProvider>
+      <SettingsProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </SettingsProvider>
+    </NotificationProvider>
+  </EmojiReactionProvider>
 )
