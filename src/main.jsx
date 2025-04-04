3dfbe157358525646d8e94f31f2f63c25570b6f5
Leonid Brown <leonid.brown01@gmail.com>
2025-04-05 00:45:01 +0700

    fix: opt

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,5 @@
-import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import UserContext, { UserProvider } from "./UserContext.jsx"
 import App from "./App.jsx"
 import "./index.css"
-import { NotificationProvider } from "./NotificationContext.jsx"
-import { SettingsProvider } from "./hooks.jsx"
-import { EmojiReactionProvider } from "./EmojiReactionContext.jsx"
-
-// config()
 
 createRoot(document.getElementById("root")).render(<App />)
